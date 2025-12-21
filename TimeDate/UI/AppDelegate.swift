fcdd574f9903d743a2714e664e2d5cd201cd9c85
Axis-Do <axis.do111@gmail.com>
2025-12-21 20:59:09 +0100

    Modification pour être prêt pour le passage en paysage de l'iPad

--- TimeDate/UI/AppDelegate.swift
+++ TimeDate/UI/AppDelegate.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     // Force la position Portrait
+    //      Force en Position Portrait Uniquement
+    //      func application(
+    //          _ application: UIApplication,
+    //            supportedInterfaceOrientationsFor window: UIWindow?
+    //        ) -> UIInterfaceOrientationMask {
+    //           return .portrait
+    //        }
+    //      }
+    
+    //  Verrouille les iPhones en Portrait mais pas les iPads
     func application(
         _ application: UIApplication,
         supportedInterfaceOrientationsFor window: UIWindow?
     ) -> UIInterfaceOrientationMask {
-        return .portrait
+        
+        guard let scene = window?.windowScene else {
+            return .portrait
+        }
+        
+        switch scene.traitCollection.userInterfaceIdiom {
+        case .phone:
+            return .portrait
+            
+        case .pad:
+            return [.portrait, .landscapeLeft, .landscapeRight]
+            
+        default:
+            return .portrait
+        }
     }
 }
