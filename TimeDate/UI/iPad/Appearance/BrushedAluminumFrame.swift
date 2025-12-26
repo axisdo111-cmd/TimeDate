428a9d69cf43a182972df82e8339dcc71cc5b6c1
Axis-Do <axis.do111@gmail.com>
2025-12-26 16:26:14 +0100

    Preparation version iPad Paysage recommencé

---
+++ TimeDate/UI/iPad/Appearance/BrushedAluminumFrame.swift
@@ -0,0 +1,8 @@
+//
+//  BrushedAluminumFrame.swift
+//  TimeDate
+//
+//  Created by Daniel PHAM-LE-THANH on 26/12/2025.
+//
+
+import Foundation
---
+++ TimeDate/UI/iPad/Appearance/CustomPresetFrame.swift
@@ -0,0 +1,8 @@
+//
+//  CustomPresetFrame.swift
+//  TimeDate
+//
+//  Created by Daniel PHAM-LE-THANH on 26/12/2025.
+//
+
+import Foundation
